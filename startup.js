680cc7ec590fc2861701a6b996db7c6175fb6af8
Jacob Millner <jacob.millner@outlook.com>
2015-06-07 20:16:14 -0400

    Grabbin' URL names and adding a debugger.

---
+++ startup.js
@@ -0,0 +1,7 @@
+//script runs when on any page at startup
+debugger;
+var jtest = $('body');
+    alert(jtest);
+    alert(jtest.text());
+var hostname = $(location).attr('hostname');
+	alert(hostname);
