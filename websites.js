e1de75c9cfa94b4dc3e729793d28211dc6de6bfd
Jacob Millner <jacob.millner@outlook.com>
2015-06-17 19:50:01 -0400

    Setting up some vars...

--- websites.js
+++ websites.js
@@ -4,11 +4,14 @@ function amazonJQ()
 {
 	debugger;
 	var buyButton = $("#add-to-cart-button");
+	var price = $("#priceblock_ourprice");
+	var oneStarRatingList = $(".a-icon a-icon-star a-star-1 review-rating");
 	
 	if(buyButton != null)
 	{
 		buyButton.parent().click(function(){
 			buyButton.attr("disabled","disabled");
+			
 			alert("dont buy this!");
 		})
 	}
