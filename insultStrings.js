9eba592b03e85212b0045e0381847431634d37b6
Jacob Millner <jacob.millner@outlook.com>
2015-07-06 18:38:16 -0400

    More insult strings.

--- insultStrings.js
+++ insultStrings.js
@@ -1,5 +1,7 @@
 var insultStrings[
 	"Don't buy this shit.",
 	"You are literally the worst.",
-	"You are literally Hitler if you buy this."
+	"You are literally Hitler if you buy this.",
+	"I'm going to tell on you...",
+	"You know you want to save that money instead."
 ];
